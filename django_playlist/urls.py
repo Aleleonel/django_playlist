9202afe9415789795baefdef9f1c78c0bc4f4bd2
aleleonel <aleleonel@gmail.com>
2018-12-07 11:55:56 -0200

    Alterações em settings.py definindo a pasta template, criação de arquivo.html exemplo, migrate ainda não realizado

--- django_playlist/urls.py
+++ django_playlist/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import about_page, home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', about_page),
+    path('', home_page),
 ]
