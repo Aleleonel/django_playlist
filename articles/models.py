93308db19e810454bcc1b5528b3af9134e3dfbbf
aleleonel <aleleonel@gmail.com>
2018-12-07 16:39:31 -0200

    Alterações no models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create your models Articles here.
+
+class Articles(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    
+
+
+
