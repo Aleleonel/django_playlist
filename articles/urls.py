78208f702033386cf41cbc6df9bccb3a9f1fa4d0
aleleonel <aleleonel@gmail.com>
2018-12-09 21:55:40 -0200

    configuração slug, logo, e links

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list
+from . import views 
 
+app_name = 'articles'
 urlpatterns = [
-    path('', article_list),
-    
+        path('', views.article_list, name= 'list'),
+        path('<slug:slug>/', views.article_detail, name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Articles
 
 def article_list(request):
     articles = Articles.objects.all().order_by('date')    
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+   return HttpResponse(slug)
+
+
