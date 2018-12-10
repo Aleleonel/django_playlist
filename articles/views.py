ca107238991657a713d27eaed54828b76bbb53ff
aleleonel <aleleonel@gmail.com>
2018-12-10 17:47:04 -0200

    Criando tela de Log in

--- articles/views.py
+++ articles/views.py
@@ -7,6 +7,10 @@ def article_list(request):
     return render(request, 'articles/article_list.html', {'articles': articles})
 
 def article_detail(request, slug):
-   return HttpResponse(slug)
+   #return HttpResponse(slug)
+   
+   article = Articles.objects.get(slug=slug)
+   return render(request, 'articles/detailpage.html', {'article':article})
+
 
 
