032115a2a26f32c1f5a0aae62a7e327e436b3c6e
aleleonel <aleleonel@gmail.com>
2018-12-12 16:51:22 -0200

    Modificações na create article - validações e forms

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Articles
 from django.contrib.auth.decorators import login_required
+from . import forms
+
 
 def article_list(request):
     articles = Articles.objects.all().order_by('date')    
@@ -15,5 +17,12 @@ def article_detail(request, slug):
 
 @login_required(login_url="/accounts/login/")
 def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            #save article to bd
+            return redirect('articles:list')     
+    else:
+        form = forms.CreateArticle()
 
-    return render(request, 'articles/article_create.html')
+    return render(request, 'articles/article_create.html', {'form':form})
